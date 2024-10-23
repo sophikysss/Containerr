6d7df666cd378cf063809861c6b4590ae1575182
Sonya <sonyagudilova254@gmail.com>
2024-10-24 01:07:18 +0300

    Update Container.java
    Создание класса Контейнер. Реализация пустого контейнера.

---
+++ Container.java
@@ -0,0 +1,13 @@
+public class Container {
+    private Object[] elems;
+    private int size;
+    private static final int initialCapacity = 10;
+
+
+    public Container() {
+        elems = new Object[initialCapacity];
+        size = 0;
+    }
+    public static void main(String[] args) {
+    }
+}
